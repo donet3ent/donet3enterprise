381c613da18d8d02b152316b21087f657108e705
JOHN TANGKOBO <55840870+gbologbolo@users.noreply.github.com>
2021-11-29 21:13:12 +0000

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+//Owl Carousel  
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHaverPause: true,
+        nav: true,
+       /*navText:[
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+       ],*/
+        responsive:{
+            0:{
+                items:2,
+                
+            },
+            600:{
+                items:2,
+                
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    })
+});
